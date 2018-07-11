72f7a2c4fbc66922a4f0cb01d810edbb7478cce8
Whitney <wkwright14@gmail.com>
2018-07-11 00:53:11 +0000

    Lesson 4 final commit

--- Lesson4_ExpenseCalculator/README.md
+++ Lesson4_ExpenseCalculator/README.md
@@ -4,22 +4,18 @@
 Web Application Development
 
 ### Assignment Date:  
-(Month Day, Year)
+(July 10, 2018)
 
 ### Student Name:  
-(First and Last Name)
+(Whitney Wright)
 
 ### Project Description:
-(In your own words, briefly describe the assignment.)
+(We learned the basic of JavaScript, developing tools, and running commands.)
 
 ### View Project:
-(Replace this statement with your GitHub Page URL that was created when you 
- published the project.)
+(ttps://wkwright14.github.io/lesson4_javascript1/)
 
 ### Lessons Learned in the Assignment:
-1. (Briefly describe a lesson/concept learned in this lesson.)
-2. (Briefly describe a lesson/concept learned in this lesson.)
-3. (Briefly describe a lesson/concept learned in this lesson.)
-
-
-
+1. (JavaScript is within a webpage to make a better design, create cookies, validate forms, and detect browsers. Because browsers are limited in their capabilities, JavaScript allows for interactivity amongst images, texts, tables, and frames.)
+2. (JavaScript can be added to HTML in two different ways: either embed the JavaScript code or embed the JavaScript in an external file. This can be done in the head or body of HTML text.)
+3. (A script is able to alter content depending on what input is given by the user or other indications, such as a server clock, hidden input, and cookies. The user's input can be informed by a dialogue box, in which the user must choose a prompt.)
--- Lesson4_ExpenseCalculator/expenseCalculator.js
+++ Lesson4_ExpenseCalculator/expenseCalculator.js
@@ -1,8 +1,8 @@
         /************************************************************************************************ 
           Course:  Web Application Development
           Assignment:  Lesson 4 (JavaScript 1)
-          Author:  
-          Date:   
+          Author: Whitney Wright
+          Date: 07/10/2018
           Filename: expenseCalculator.js
         *************************************************************************************************/
      
@@ -36,7 +36,9 @@
 
                   var shelterAmt = document.getElementById("shelterInput").value;
                   var foodAmt = document.getElementById("foodInput").value;
-
+                var householdAmt = document.getElementById("householdInput").value;
+                var transportationAmt = document.getElementById("transportationInput").value;
+                var miscellaneousAmt = document.getElementById("miscellaneousInput").value;
 
 
 
@@ -58,7 +60,9 @@
 
                   shelterAmt = parseFloat(shelterAmt);
                   foodAmt = parseFloat(foodAmt);
-
+                transportationAmt = parseFloat(transportationAmt);
+                householdAmt = parseFloat(householdAmt);
+                miscellaneousAmt = parseFloat(miscellaneousAmt);
 
 
 
@@ -79,6 +83,9 @@
                   var totalExpenses; //total expenses for the user
                   var shelterPct; //shelter category percentage
                   var foodPct;  //food category percentage
+                  var householdPct;  //household category percentage
+                  var transportationPct;  //transportation category percentage
+                  var miscellaneousPct;  //miscellaneous category percentage
 
 
 
@@ -105,10 +112,14 @@
                    *          Miscellaneous percentage (you must add). The result of the calculations are assigned to the 
                    *          variables declared in Task 4.  
                   *******************************************************************************************************/   
-
+                    
+                    totalExpenses = shelterAmt + foodAmt + householdAmt + transportationAmt + miscellaneousAmt;
 
                   shelterPct = 100 * (shelterAmt / totalExpenses);
                   foodPct = 100 * (foodAmt / totalExpenses);
+                  householdPct = 100 * (householdAmt / totalExpenses);
+                  transportationPct = 100 * (transportationAmt / totalExpenses);
+                  miscellaneousPct = 100 * (miscellaneousAmt / totalExpenses);
 
 
                   /******************************************************************************************************
@@ -135,8 +146,9 @@
                   document.getElementById("totalExpenses").innerHTML = "<b>Total Expenses:     $</b> " + totalExpenses.toFixed(2);
                   document.getElementById("shelterPercentage").innerHTML = "% for <b>shelter</b>:      " + shelterPct.toFixed(1);      
                   document.getElementById("foodPercentage").innerHTML = "% for <b>food:</b>      " + foodPct.toFixed(1);
-
-
+                document.getElementById("transportationPercentage").innerHTML = "% for <b>transportation:</b>      " + transportationPct.toFixed(1);
+                document.getElementById("householdPercentage").innerHTML = "% for <b>housebold:</b>      " + householdPct.toFixed(1);
+                document.getElementById("miscellaneousPercentage").innerHTML = "% for <b>miscellaneous:</b>      " + miscellaneousPct.toFixed(1);
          } //end of calculatePercentages function
          
 
@@ -171,7 +183,9 @@
                   document.getElementById("totalExpenses").innerHTML = ""; 
                   document.getElementById("shelterPercentage").innerHTML = "";      
                   document.getElementById("foodPercentage").innerHTML  = "";
-
+                document.getElementById("householdPercentage").innerHTML  = "";
+                document.getElementById("transportationPercentage").innerHTML  = "";
+                document.getElementById("miscellaneousPercentage").innerHTML  = "";
 
               
          }//end of clearForm function
